44e56a3dc0dd675b98627a64d2b06bc93a349524
Hkc-1673 <habibkc71@gmail.com>
2020-12-14 23:06:58 +0200

    Create server and add routers

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+require('dotenv').config()
+
+//import Routes
+const authRoute = require("./routes/auth");
+
+//Connect DB
+mongoose.connect(process.env.MONGODB_URI, 
+    {useNewUrlParser: true,
+    useUnifiedTopology: true},
+    () => console.log("connected DB")
+);
+//Middleware
+app.use(express.json());
+
+
+//Route Middleware
+
+app.use("/api/user", authRoute);
+
+
+app.listen(3000, () => {
+    console.log("I'm listening on port")
+});
